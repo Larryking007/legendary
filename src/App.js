f0d4feaee830a1d521168f52708e046e9d049db2
Larryking007 <ajikobilanre007@gmail.com>
2022-08-21 04:17:08 +0100

    current weather and forecast

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
+import { useState } from 'react';
+import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
 import './App.css';
 import Search from './components/search/search';
 const Currentweather = require('../src/components/search/current-weather/current-weather');
+const { WEATHER_API_URL, WEATHER_API_KEY }  = require('./api');
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForeCast] = useState(null);
+
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ")
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const foreCastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+
+    Promise.all([currentWeatherFetch, foreCastFetch])
+    .then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const foreCastResponse = await response[1].json();
+
+      setCurrentWeather({city: searchData.label, ...weatherResponse});
+      setForeCast({city: searchData.label, ...foreCastResponse});
+    })
+    .catch((err) => console.log(err));
   }
 
+  console.log(currentWeather);
+  console.log(forecast);
   return (
     <div className="container">
       <Search onSearchChange = {handleOnSearchChange}/>
-      <Currentweather />
+      {currentWeather && <Currentweather data={currentWeather}/>}
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -7,3 +7,6 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+export const WEATHER_API_KEY = "0e3c17ddd864ad39108b88b5c24946f2";
